bafc9fb6045a20221cd07d282d01fe2831d7c49e
Noah Huber-Feely <nhuberfeely@gmail.com>
2015-06-23 10:38:52 -0500

    Added comments

--- LightGUI.py
+++ LightGUI.py
@@ -2,37 +2,56 @@
 from Tkinter import *
 import bluetooth
 
+#Look for all Bluetooth devices
+#the computer knows about.
 print "Searching for devices..."
 print ""
+#Create an array with all the MAC
+#addresses of the detected devices
 nearby_devices = bluetooth.discover_devices()
+#Run through all the devices found and list their name
 num = 0
 print "Select your device by entering its coresponding number..."
 for i in nearby_devices:
 	num+=1
 	print num , ": " , bluetooth.lookup_name( i )
 
+#Allow the user to select their Arduino
+#bluetooth module. They must have paired
+#it before hand.
 selection = input("> ") - 1
 print "You have selected", bluetooth.lookup_name(nearby_devices[selection])
 bd_addr = nearby_devices[selection]
 
 port = 1
 
+#Create the GUI
 class Application(Frame):
 
+#Create a connection to the socket for Bluetooth
+#communication
     sock = bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
     def disconnect(self):
+    	#Close socket connection to device
         self.sock.close()
         
     def on(self):
+    	#Send 'H' which the Arduino
+    	#detects as turning the light on
         data = "H"
         self.sock.send(data)
 
     def off(self):
+    	#Send 'L' to turn off the light
+    	#attached to the Arduino
         data = "L"
         self.sock.send(data)
 
     def createWidgets(self):
+    	#Form all the buttons.
+    	#Look at a Tkinter reference
+    	#for explanations.
         self.QUIT = Button(self)
         self.QUIT["text"] = "QUIT"
         self.QUIT["fg"]   = "red"
@@ -62,11 +81,14 @@ class Application(Frame):
         self.turnOff.pack({"side": "left"})
 
     def __init__(self, master=None):
+    	#Connect to the bluetooth device
+    	#and initialize the GUI
         self.sock.connect((bd_addr, port))
         Frame.__init__(self, master)
         self.pack()
         self.createWidgets()
 
+#Begin the GUI processing
 root = Tk()
 app = Application(master=root)
 app.mainloop()
